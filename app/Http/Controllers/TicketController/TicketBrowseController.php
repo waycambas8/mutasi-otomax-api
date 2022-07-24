6a0f727bf1c4b15e483f915979461a5df9b0fe65
MULTYcode <mhdwasiman@gmail.com>
2022-07-24 18:37:24 +0700

    Fail Update type in ticket

--- app/Http/Controllers/TicketController/TicketBrowseController.php
+++ app/Http/Controllers/TicketController/TicketBrowseController.php
@@ -23,12 +23,13 @@ class TicketBrowseController extends Controller
         $date_from = Carbon::parse($request->get('date_from'))->startOfDay();
         $date_end = Carbon::parse($request->get('date_end'))->endOfDay();
 
+
         $response['count'] = Ticket::select('count(*) as allcount')
                 ->join("inbox","inbox.kode","tiket_deposit.kode_inbox")
                 ->leftJoin("data_bank","tiket_deposit.kode_data_bank","data_bank.kode")
                 ->where("tiket_deposit.kode_reseller",$this->kode)
-                ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end])
-                ->count();
+                ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end]);
+
         $response['totalRecordswithFilter'] = Ticket::where("tiket_deposit.kode_reseller",$this->kode)
                     ->join("inbox","inbox.kode","tiket_deposit.kode_inbox")
                     ->leftJoin("data_bank","tiket_deposit.kode_data_bank","data_bank.kode")
@@ -38,10 +39,16 @@ class TicketBrowseController extends Controller
                             ->orWhere("tiket_deposit.status","like","%".$searchValue."%")
                             ->orWhere("tiket_deposit.kode_data_bank","like","%".$searchValue."%");
                     })
-                    ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end])
-                    ->count();
-        
-        $response['records'] = Ticket::select("inbox.status as status_inbox","inbox.pesan as pesan","tiket_deposit.*","data_bank.kode as kode_bank","data_bank.bank as bank")->orderBy("tiket_deposit.".$this->columnName,$this->columnSortOrder)
+                    ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end]);
+                            
+        $response['records'] = Ticket::select(
+            "inbox.status as status_inbox",
+            "inbox.pesan as pesan","tiket_deposit.*",
+            "data_bank.kode as kode_bank",
+            "data_bank.bank as bank", 
+            "inbox.kode as kode_inbox"
+            //"transaksi.kode as kode_transaksi"
+            )->orderBy("tiket_deposit.".$this->columnName,$this->columnSortOrder)
                     ->join("inbox","inbox.kode","tiket_deposit.kode_inbox")
                     ->leftJoin("data_bank","tiket_deposit.kode_data_bank","data_bank.kode")
                     ->where("tiket_deposit.kode_reseller",$this->kode)
@@ -51,11 +58,18 @@ class TicketBrowseController extends Controller
                             ->orWhere("tiket_deposit.status","like","%".$searchValue."%")
                             ->orWhere("tiket_deposit.kode_data_bank","like","%".$searchValue."%");
                     })
-                    ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end])
-                    ->skip($this->start)
-                    ->take($this->rowperpage)
-                    ->get()->toArray();
+                    ->whereBetween("tiket_deposit.tgl_status",[$date_from,$date_end]);
+                    
+        if (!empty($request->type)){
+            $response['count'] = $response['count']->leftjoin("transaksi","transaksi.kode_inbox","inbox.kode");
+            $response['totalRecordswithFilter'] = $response['totalRecordswithFilter']->leftjoin("transaksi","transaksi.kode_inbox","inbox.kode");
+            $response['records'] = $response['records']->leftjoin("transaksi","transaksi.kode_inbox","inbox.kode");
+        }
         
+        $response['count'] = $response['count']->count();
+        $response['totalRecordswithFilter'] = $response['totalRecordswithFilter']->count();
+        $response['records'] = $response['records']->skip($this->start)->take($this->rowperpage)->get()->toArray();
+
         $response['status'] = $this->ticket_status()->status;
 
         return fractal()
